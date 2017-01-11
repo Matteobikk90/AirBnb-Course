ca1dc74c52bbf7dca4779a6fe9c2e855cfd74918
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2017-01-11 23:39:24 +0100

    Bootstrapping logIn/signUp section

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   protected
   	def configure_permitted_parameters
-  		devise_parameters_sanitazer.for(:sign_up) << :fullname
-  		devise_parameters_sanitazer.for(:account_update) << :fullname
+  		devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
 	end
 end
