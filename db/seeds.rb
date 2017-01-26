42677305e840775ee7dcc6fd63a88838f7d6df24
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2017-01-26 11:06:15 +0100

    Booking form restyling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# User.create(:email => 'test@gmail.com', :password => '123456789', :fullname => 'Test App');
+
+# Room.create(:id => 1, :home_type => 'House',           :room_type => 'Entire',  :accomodate => 3,  :bed_room => 3, :bath_room => 2, :listing_name => 'Archway House',    :summary => 'Amazing house in Archway, north London, very near the station, 3 floors and 2 minutes walk from tube.', :address => '15 Bovingdone close, N195SN, London', :is_tv => true, :is_kitchen => true, :is_air => 'false', :is_heating => false, :is_internet => true, :price => 200, :active => true, :user_id => 1);
+# Room.create(:id => 2, :home_type => 'Bed & Breakfast', :room_type => 'Shared',  :accomodate => 4,  :bed_room => 3, :bath_room => 1, :listing_name => 'Queens House',     :summary => 'Amazing house in Archway, north London, very near the station, 3 floors and 2 minutes walk from tube.', :address => '15 Bovingdone close, N195SN, London', :is_tv => true, :is_kitchen => true, :is_air => 'false', :is_heating => false, :is_internet => true, :price => 200, :active => true, :user_id => 1);
+# Room.create(:id => 3, :home_type => 'Apartment',       :room_type => 'Private', :accomodate => 6+, :bed_room => 3, :bath_room => 5, :listing_name => 'Rosko House',      :summary => 'Amazing house in Archway, north London, very near the station, 3 floors and 2 minutes walk from tube.', :address => '15 Bovingdone close, N195SN, London', :is_tv => true, :is_kitchen => true, :is_air => 'false', :is_heating => false, :is_internet => true, :price => 200, :active => true, :user_id => 1);
+# Room.create(:id => 4, :home_type => 'House', 		   :room_type => 'Shared',  :accomodate => 2,  :bed_room => 3, :bath_room => 3, :listing_name => 'Parliament House', :summary => 'Amazing house in Archway, north London, very near the station, 3 floors and 2 minutes walk from tube.', :address => '15 Bovingdone close, N195SN, London', :is_tv => true, :is_kitchen => true, :is_air => 'false', :is_heating => false, :is_internet => true, :price => 200, :active => true, :user_id => 1);
